e50a844d521ac2324785aa9562d3022f8be19ada
rajaram-parab <30703171+rajaram-parab@users.noreply.github.com>
2017-08-05 20:37:40 +0530

    Accepting Arguments in Console

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(String arg : args)
+		{
+			System.out.println(arg + " k"+ arg.length());
+		}
+
+	}
+
+}
